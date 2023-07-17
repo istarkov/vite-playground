f920a1c13dc67fc132eecaba182d1ddc96675035
istarkov <istarkov@gmail.com>
2023-07-17 15:13:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto]">
+      <div className="bg-red-500 h-20">1</div>
+      <div>CONTENT</div>
+      <div className="bg-red-500 h-20">3</div>
+    </div>
+  );
+}
+
+export default App;
