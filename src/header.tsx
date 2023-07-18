9e5915a2854c31e5e96d6c4a71d7ff46cb124e71
istarkov <istarkov@gmail.com>
2023-07-18 20:49:03 +0300

    Test

--- src/header.tsx
+++ src/header.tsx
@@ -12,16 +12,32 @@ import {
 export const Header = () => {
   return (
     <div className="grid justify-items-center">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem className="relative">
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent className="w-full">
-              <NavigationMenuLink>Link</NavigationMenuLink>
+      <NavigationMenu data-tag="NavigationMenu">
+        <NavigationMenuList data-tag="NavigationMenuList">
+          <NavigationMenuItem data-tag="NavigationMenuItem">
+            <NavigationMenuTrigger data-tag="NavigationMenuTrigger">
+              Getting started
+            </NavigationMenuTrigger>
+            <NavigationMenuContent data-tag="NavigationMenuContent">
+              <div className="flex gap-2 m-4">
+                <div className="min-w-0 flex-1">
+                  <NavigationMenuLink data-tag="NavigationMenuLink" href="/">
+                    Introduction
+                  </NavigationMenuLink>
+                  <div className="text-sm text-gray-500">
+                    Although the phrase is nonsense, it does have a long history
+                  </div>
+                </div>
+                <div className="min-w-0 flex-1">
+                  <NavigationMenuLink href="/">Link 1.2</NavigationMenuLink>
+                  <NavigationMenuLink href="/">Link 1.3</NavigationMenuLink>
+                  <NavigationMenuLink href="/">Link 1.4</NavigationMenuLink>
+                </div>
+              </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
 
-          <NavigationMenuItem className="relative">
+          <NavigationMenuItem>
             <NavigationMenuTrigger>Item 2</NavigationMenuTrigger>
             <NavigationMenuContent>
               <NavigationMenuLink>Link 1</NavigationMenuLink>
